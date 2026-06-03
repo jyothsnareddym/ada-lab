af83bbc49200625531fa205c81b6f0161a99202d
jyothsnareddym <jyothsnareddy.cs24@bmsce.ac.in>
2026-06-03 15:49:24 +0530

    Implement topological sort algorithm in C

---
+++ Topologicalsort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define MAX 10
+int graph[MAX][MAX], indegree[MAX], queue[MAX];
+int n;
+void topologicalSort() {
+    int front = 0, rear = 0;
+    int count = 0, i, j;
+    for (i = 0; i < n; i++) {
+        indegree[i] = 0;
+        for (j = 0; j < n; j++)
+            indegree[i] += graph[j][i];
+    }
+    for (i = 0; i < n; i++)
+        if (indegree[i] == 0)
+            queue[rear++] = i;
+    printf("Topological Ordering: ");
+    while (front < rear) {
+        int v = queue[front++];
+        printf("%d ", v);
+        count++;
+        for (i = 0; i < n; i++)
+            if (graph[v][i] == 1) {
+                indegree[i]--;
+                if (indegree[i] == 0)
+                    queue[rear++] = i;
+            }
+       
+    }
+    if (count != n) {
+        printf("\nGraph contains a cycle. Topological sort not possible.\n");
+    }
+}
+int main() {
+    int i, j;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    topologicalSort();
+    return 0;
+}
